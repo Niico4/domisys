b8184245fb2276a08591b538a019dbb38ba57b8a
Niico4 <nicogarzon131@gmail.com>
2024-12-18 19:04:42 -0500

    feat(root): initial setup with installing essential tools

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/react';
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'bg-custom-gradient':
+          'linear-gradient(to left, #7db8ff 0%, #d8a7ff 100%)',
+        'text-custom-gradient':
+          'linear-gradient(40deg, #00fff7 0%, #ae4aff 100%)',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: 'transparent',
+        secondary: '#c176fe',
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 } satisfies Config;
