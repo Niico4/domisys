6ce3c868f90233fbe03e587fbbdf5026238153d1
Niico4 <nicogarzon131@gmail.com>
2025-04-19 23:05:32 -0500

    feat(api): configure laravel as api-only

--- apps/client/src/pages/delivery/index.tsx
+++ apps/client/src/pages/delivery/index.tsx
@@ -1,8 +1,30 @@
+import { Navigate } from 'react-router-dom';
+import { Card } from '@heroui/card';
+import { IconPackage } from '@tabler/icons-react';
+
+import useAuth from '@/hooks/useAuth';
+
 const Home = () => {
+  const { user } = useAuth();
+  if (!user) return <Navigate to="/login" replace={true} />;
+
   return (
-    <div>
-      <h1>DELIVERY</h1>
-    </div>
+    <section>
+      <h1>¡Bienvenido, {user.name}!</h1>
+
+      <article>
+        <Card className="bg-glass" radius="sm">
+          <div>
+            <p>16</p>
+            <span>Pedidos Activos</span>
+          </div>
+
+          <div className="p-4 rounded-full bg-warning/20">
+            <IconPackage className="text-warning-50" stroke={1.5} size={32} />
+          </div>
+        </Card>
+      </article>
+    </section>
   );
 };
 
